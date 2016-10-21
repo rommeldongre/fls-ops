00f58f8ae50aff5586d56d47eea0f981395a71c3
rommel <rommel@greylabs.org>
2016-10-21 13:58:51 +0530

    deploy path

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 echo "+++Deploy source in $PWD+++"
 
 echo "+++ Shutting down Tomcat ..."
-/Applications/tomcatstack-7.0.57-0/ctlscript.sh stop 
+/Applications/tomcatstack-8.0.38-0/ctlscript.sh stop 
 sleep 5
 
 echo "+++ Installing new war ..."
-rm -rf /Applications/tomcatstack-7.0.57-0/apache-tomcat/webapps/fls-ops*
-cp build/fls-ops.war /Applications/tomcatstack-7.0.57-0/apache-tomcat/webapps/
+rm -rf /Applications/tomcatstack-8.0.38-0/apache-tomcat/webapps/fls-ops*
+cp build/fls-ops.war /Applications/tomcatstack-8.0.38-0/apache-tomcat/webapps/
 
 echo "+++ Starting up Tomcat ..."
-rm /Applications/tomcatstack-7.0.57-0/apache-tomcat/logs/*
-/Applications/tomcatstack-7.0.57-0/ctlscript.sh start 
+rm /Applications/tomcatstack-8.0.38-0/apache-tomcat/logs/*
+/Applications/tomcatstack-8.0.38-0/ctlscript.sh start 
 sleep 5
 
 
