3d5baf3254fd667cec794b752eeb091adfa22fa6
Ankit Karnany <ankit@greylabs.org>
2017-03-23 18:04:55 +0530

    add apis for tickets

--- WebContent/js/tickets.js
+++ WebContent/js/tickets.js
@@ -18,6 +18,37 @@ ticketsApp.controller('ticketsCtrl', ['$scope', 'ticketsApi', function ($scope, 
         );
     }
 
+    $scope.addTicket = function () {
+        ticketsApi.addTicket({
+            ticketUserId: "ankit@greylabs.org",
+            dueDate: null,
+            ticketType: "GENERAL"
+        }).then(
+            function (res) {
+                var response = res.data;
+                console.log(response);
+            },
+            function (response) {
+                console.log(response);
+            }
+        );
+    }
+
+    $scope.addNote = function () {
+        ticketsApi.addNote({
+            note: "This is a note",
+            ticketId: 3
+        }).then(
+            function (res) {
+                var response = res.data;
+                console.log(response);
+            },
+            function (response) {
+                console.log(response);
+            }
+        );
+    }
+
 }]);
 
 ticketsApp.service('ticketsApi', ['$http', 'loginService', function ($http, loginService) {
@@ -30,6 +61,16 @@ ticketsApp.service('ticketsApi', ['$http', 'loginService', function ($http, logi
     this.addTicketType = function (params) {
         angular.extend(req, params);
         return $http.post('/AddTicketType', JSON.stringify(req));
+    }
+
+    this.addTicket = function (params) {
+        angular.extend(req, params);
+        return $http.post('/AddTicket', JSON.stringify(req));
+    }
+
+    this.addNote = function (params) {
+        angular.extend(req, params);
+        return $http.post('/AddNote', JSON.stringify(req));
     }
 
 }]);
